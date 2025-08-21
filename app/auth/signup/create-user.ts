fc1fac75b8757b4548bbb2ef49248fe3b4dcc76d
ZERIN <a.r.zerin@gmail.com>
2025-08-22 00:38:53 +0600

    Server Action

--- app/auth/signup/create-user.ts
+++ app/auth/signup/create-user.ts
@@ -5,11 +5,28 @@ import { getErrorMessage } from "@/app/util/errors";
 import { redirect } from "next/navigation";
 
 export default async function createUser(_prevState: any, formData: FormData) {
+  
+  console.log('Form Data', formData);  
+  
+  console.log(`FORM Action URL: ${API_URL}/users`);
+
+  const email = String(formData.get('email') ?? '');
+  const password = String(formData.get('password') ?? '');
+
+  console.log(`Email: ${email}`);
+  console.log(`Password: ${password}`);
+
   const res = await fetch(`${API_URL}/users`, {
     method: "POST",
-    body: formData,
+    headers: { 'Content-Type': 'application/json' },
+    //body: formData,
+    body: JSON.stringify({ email, password }),
   });
+  
   const parsedRes = await res.json();
+  
+  console.log('Response:', parsedRes);
+
   if (!res.ok) {
     return { error: getErrorMessage(parsedRes) };
   }
